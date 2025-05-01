86ffed0072dbf3e435d04be7a5d98029569b39c6
Zeeton <146897841+Dhanush-67@users.noreply.github.com>
2025-05-01 13:55:04 -0400

    Added a new project

---
+++ flavour_determiner_graph/summer_plans_1011401305.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(void){
+    printf("This summer, Dhanush is planning to:Do a summer term consisting of 3 courses, 1 of which is MATA37 and the other 2 are electives.\n");
+}
